1173e3e9b8c1add060d7ca858d155e9ac7c5ebe3
Guilherme Henrique <ghenrique@inpower-sp.local>
2025-03-18 07:31:33 -0300

    Feat: Add New Query

---
+++ Departamentos/Custom/C_Transferencias_Matriz.sql
@@ -0,0 +1,34 @@
+SELECT 
+    T0."DocNum", 
+    T0."DocDate",
+    T0."VATRegNum", 
+    T2."Usage", 
+    SUM(T3."LineTotal") AS "Total Linha", 
+    T0."Serial", 
+    Z4."CreateDate" AS "Data de Criação",
+    CAST(T0."Header" AS NVARCHAR(255)) AS "Abertura", 
+    CAST(T0."Footer" AS NVARCHAR(255)) AS "Encerramento"
+FROM OINV T0 
+INNER JOIN INV12 T1 ON T0."DocEntry" = T1."DocEntry" 
+INNER JOIN OUSG T2 ON T1."MainUsage" = T2."ID" 
+INNER JOIN INV1 T3 ON T0."DocEntry" = T3."DocEntry"
+LEFT JOIN OPCH Z4 
+    ON T0."Serial" = Z4."Serial"
+    AND (
+        (T0."VATRegNum" = '17.642.282/0005-57' AND Z4."CardCode" = 'F001577') OR
+        (T0."VATRegNum" = '17.642.282/0007-19' AND Z4."CardCode" = 'F002550') OR
+        (T0."VATRegNum" = '17.642.282/0004-76' AND Z4."CardCode" = 'F002085') OR
+        (T0."VATRegNum" = '17.642.282/0003-95' AND Z4."CardCode" = 'F002550') 
+    )
+WHERE 
+    T0."CardCode" = 'CLJ001' 
+    AND T0."CANCELED" = 'N'  
+    AND T2."ID" = 47 
+    AND T0."DocDate" >= '2025-01-01' 
+    AND Z4."CreateDate" IS NULL  
+    AND T0."VATRegNum" IN ('17.642.282/0005-57', '17.642.282/0007-19', '17.642.282/0004-76', '17.642.282/0003-95')  
+GROUP BY 
+    T0."DocNum", T0."DocDate", T0."VATRegNum", 
+    T2."Usage", T0."Serial", Z4."CreateDate", 
+    CAST(T0."Header" AS NVARCHAR(255)), CAST(T0."Footer" AS NVARCHAR(255))  
+ORDER BY "Total Linha" DESC;
