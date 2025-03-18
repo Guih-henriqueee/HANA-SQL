6cd5fb6a553b819b62cafb44eb1ccc63dec1a716
Guilherme Henrique <ghenrique@inpower-sp.local>
2025-03-18 07:54:57 -0300

    Feat: Update C Transferencias Matriz

--- Departamentos/Custom/C_Transferencias_Matriz.sql
+++ Departamentos/Custom/C_Transferencias_Matriz.sql
@@ -5,13 +5,17 @@ SELECT
     T2."Usage", 
     SUM(T3."LineTotal") AS "Total Linha", 
     T0."Serial", 
-    Z4."CreateDate" AS "Data de Criação",
+    Z4."CreateDate" AS "Data de Criação NE",
+    Z5."CreateDate" AS "Data de Criação DS",
     CAST(T0."Header" AS NVARCHAR(255)) AS "Abertura", 
-    CAST(T0."Footer" AS NVARCHAR(255)) AS "Encerramento"
+    CAST(T0."Footer" AS NVARCHAR(255)) AS "Encerramento", 
+    Z0."U_NAME" AS "Usuário"
 FROM OINV T0 
 INNER JOIN INV12 T1 ON T0."DocEntry" = T1."DocEntry" 
 INNER JOIN OUSG T2 ON T1."MainUsage" = T2."ID" 
 INNER JOIN INV1 T3 ON T0."DocEntry" = T3."DocEntry"
+INNER JOIN OUSR Z0 ON T0."UserSign" = Z0."USERID"
+LEFT JOIN OPCH Z5 ON T0."Serial"  = Z5."Serial"
 LEFT JOIN OPCH Z4 
     ON T0."Serial" = Z4."Serial"
     AND (
@@ -25,10 +29,10 @@ WHERE
     AND T0."CANCELED" = 'N'  
     AND T2."ID" = 47 
     AND T0."DocDate" >= '2025-01-01' 
-    AND Z4."CreateDate" IS NULL  
     AND T0."VATRegNum" IN ('17.642.282/0005-57', '17.642.282/0007-19', '17.642.282/0004-76', '17.642.282/0003-95')  
 GROUP BY 
     T0."DocNum", T0."DocDate", T0."VATRegNum", 
-    T2."Usage", T0."Serial", Z4."CreateDate", 
-    CAST(T0."Header" AS NVARCHAR(255)), CAST(T0."Footer" AS NVARCHAR(255))  
+    T2."Usage", T0."Serial", Z4."CreateDate",Z5."CreateDate", 
+    CAST(T0."Header" AS NVARCHAR(255)), CAST(T0."Footer" AS NVARCHAR(255)),
+    Z0."U_NAME"
 ORDER BY "Total Linha" DESC;
