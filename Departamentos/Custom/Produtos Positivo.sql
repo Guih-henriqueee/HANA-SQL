feb79e01d7bc90ee2f650877b5ce8cc122b00122
Joao Felipe Ferreira Santana <joao.felipe@inpower-sp.local>
2025-03-25 17:09:00 -0300

    Atualização na query da Positivo

--- Departamentos/Custom/Produtos Positivo.sql
+++ Departamentos/Custom/Produtos Positivo.sql
@@ -4,18 +4,19 @@ SELECT
     T1."ItemCode", 
     T3."ItemName", 
     SUM(T1."LineTotal") AS "Valor da Nota", 
-    SUM(T1."Quantity") AS "Quantidade", 
+    T1."Quantity" AS "Quantidade", 
     T0."Serial" AS "Nota Fiscal", 
-    T0."U_chaveacesso" AS "Chave de acesso", 
+    CASE WHEN T0."U_chaveacesso" IS NOT NULL THEN T0."U_chaveacesso" ELSE T7."U_ChaveAcesso" END AS "Chave de acesso", 
     T2."SlpName" AS "Vendedor", 
     NULL AS "CPF" 
-FROM OINV T0
-INNER JOIN INV1 T1 ON T0."DocEntry" = T1."DocEntry"
-INNER JOIN OSLP T2 ON T0."SlpCode" = T2."SlpCode"
-INNER JOIN OITM T3 ON T1."ItemCode" = T3."ItemCode"
-INNER JOIN OITB T4 ON T3."ItmsGrpCod" = T4."ItmsGrpCod"
-INNER JOIN OMRC T5 ON T3."FirmCode" = T5."FirmCode"
-LEFT JOIN OKS_NFS T6 ON T0."DocEntry" = T6."DocEntry"
+FROM SBO_INPOWER_PROD."OINV" T0
+INNER JOIN SBO_INPOWER_PROD."INV1" T1 ON T0."DocEntry" = T1."DocEntry"
+INNER JOIN SBO_INPOWER_PROD."OSLP" T2 ON T0."SlpCode" = T2."SlpCode"
+INNER JOIN SBO_INPOWER_PROD."OITM" T3 ON T1."ItemCode" = T3."ItemCode"
+INNER JOIN SBO_INPOWER_PROD."OITB" T4 ON T3."ItmsGrpCod" = T4."ItmsGrpCod"
+INNER JOIN SBO_INPOWER_PROD."OMRC" T5 ON T3."FirmCode" = T5."FirmCode"
+LEFT JOIN SBO_INPOWER_PROD."OKS_NFS" T6 ON T0."DocEntry" = T6."DocEntry"
+LEFT JOIN SBO_INPOWER_PROD."@SKL25NFE" T7 ON T0."DocEntry" = T7."U_DocEntry"
 WHERE 
     (T5."FirmName" LIKE '%Infi%' OR T5."FirmName" LIKE '%VAIO%') 
     AND T4."ItmsGrpNam" IN ('Tablets', 'Celular e Telefone') 
@@ -29,7 +30,9 @@ GROUP BY
     T3."ItemName", 
     T0."Serial", 
     T0."U_chaveacesso", 
-    T2."SlpName"
+    T7."U_ChaveAcesso",
+    T2."SlpName",
+    T1."Quantity"
 ORDER BY 
     T0."DocNum", 
-    T0."DocDate";
+    T0."DocDate"
